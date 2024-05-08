3f56ca68e5281729ed6d3d38bca9bd7a89e6d751
HBK.theKid <101424078+Fx-Tree@users.noreply.github.com>
2024-05-08 17:23:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 const contact = document.querySelector(".contact-circle");
 const modal = document.querySelector(".section1");
 const overlay = document.querySelector(".overlay");
+const contact2 = document.querySelector(".content-div");
 function showmodal() {
   modal.classList.remove("hide");
 }
 function hideModal() {
   modal.classList.add("hide");
 }
+
+contact2.addEventListener("click", showmodal);
 
 contact.addEventListener("click", showmodal);
 
